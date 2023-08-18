1aac70f5f18ae58b1d7103701fc866cf13eb4ccb
Alina Komissarova <mohinalina@gmail.com>
2023-08-18 19:04:46 +0200

    Add task3 v2

--- task3.py
+++ task3.py
@@ -1,10 +1,18 @@
 arr = list(map(int, input().split()))
 n = len(arr)
 
+# for i in range(n):
+#     j = i
+#     while j > 0 and arr[j - 1] > arr[j]:
+#         arr[j - 1], arr[j] = arr[j], arr[j - 1]
+#         j -= 1
+#         print(arr)
+
 for i in range(n):
-    j = i
-    while j > 0 and arr[j - 1] > arr[j]:
-        arr[j - 1], arr[j] = arr[j], arr[j - 1]
-        j -= 1
+    for j in range(0, i):
+        if arr[j] > arr[i]:
+            elem = arr.pop(i)
+            arr.insert(j, elem)
+            break
 
 print(arr)
